9c76b61241bcacd5328c87e567915378a8cf3e40
Kariuki11 <irungukenneth433@gmail.com>
2024-06-04 17:09:24 +0300

    JS Practise

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start(){
+
+    if(isRunning){
+
+    }
+
+}
+
+function stop(){
+
+}
+
+function reset(){
+
+}
+
+function update(){
+
+}
